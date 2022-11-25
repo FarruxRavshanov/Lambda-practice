0963903355ade7908409ece2487da9a9cf3f1448
FarruxRavshanov <farruxlynx@gmail.com>
2022-11-25 14:41:34 +0300

    test

--- lambda_advanced.py
+++ lambda_advanced.py
@@ -13,7 +13,11 @@ def max_odd(l: list) -> int:
     Returns:
         int: maximum odd number
     '''
-
+    odd = []
+    for i in l:
+        if i % 2 == 1:
+            odd.append(i)
+    lambda l: max(odd)
 
 def min_odd(l: list) -> int:
     '''returns the minimum odd number in the list 'l' given as an argument. (you may use min, labmda)
@@ -24,7 +28,11 @@ def min_odd(l: list) -> int:
     Returns:
         int: maximum odd number
     '''
-
+    odd = []
+    for i in l:
+        if i % 2 == 1:
+            odd.append(i)
+    lambda l: min(odd)
 
 def max_even(l: list) -> int:
     '''returns the maximum even number in the list 'l' given as an argument. (you may use max, labmda)
@@ -35,8 +43,12 @@ def max_even(l: list) -> int:
     Returns:
         int: maximum even number
     '''
-
-
+    even = []
+    for s in l:
+        if s % 2 == 0:
+            even.append(s)
+    
+    lambda l: max(even)
 def min_even(l: list) -> int:
     '''returns the minimum even number in the list 'l' given as an argument. (you may use min, labmda)
     
@@ -46,7 +58,12 @@ def min_even(l: list) -> int:
     Returns:
         int: maximum even number
     '''
-
+    even = []
+    for s in l:
+        if s % 2 == 0:
+            even.append(s)
+    
+    lambda l: min(even)
 
 def oldest_user(users: list[dict]) -> dict:
     '''returns the oldest user in the list 'users' given as an argument. (you may use max, labmda)
@@ -57,7 +74,11 @@ def oldest_user(users: list[dict]) -> dict:
     Returns:
         dict: the oldest user
     '''
-
+    ages = []
+    for age in l["users"]["age"]:
+        if age[0] < age:
+            old = age
+    return old
 
 def youngest_user(users: list[dict]) -> dict:
     '''returns the youngest user in the list 'users' given as an argument. (you may use min, labmda)
@@ -68,3 +89,8 @@ def youngest_user(users: list[dict]) -> dict:
     Returns:
         dict: the youngest user
     '''
+    ages = []
+    for age in l["users"]["age"]:
+        if age[0] > age:
+            young = age
+    return young
